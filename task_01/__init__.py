cb0f36dfb3cf2210d91d9b9b509c405a7c1df53b
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-03-06 04:26:47 +0000

    Wk 6 package creation- init file

---
+++ task_01/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Package creation for module 1,
+   named Peanut
+"""
+
---
+++ task_01/peanut.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" A new module part of a package"""
+
+BUTTER = True
+
+OIL = False
